14b04751462cb478fa92dfac66f692165ad743b1
GalinaKolp2018 <kggala@mail.ru>
2018-11-04 12:08:27 +0300

    фото инстаграмм

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){ 
 
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '6518360018',
+        accessToken: '6518360018.1677ed0.1823975fb4044acc91b443b629938889',
+        resolution: 'standard_resolution',
+        limit: 33
+    });
+   
+    userFeed.run();
+    
+    
+    setTimeout(function(){
+        
+        $('.instafeed a').addClass('.shadowed');
+    }, 100 );
 
 
 });
